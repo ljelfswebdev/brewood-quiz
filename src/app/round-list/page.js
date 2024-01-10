1efa3a34b245eee3a846ce63631f7e3dcb164bec
Lewis <lewis@westmidlandsmedia.co.uk>
2024-01-10 18:36:01 +0000

    Carey Quiz

--- src/app/round-list/page.js
+++ src/app/round-list/page.js
@@ -2,28 +2,28 @@ const RoundList = () => {
     return ( 
         <ul className="page__list">
             <li>
-                Round 1 - Sport
+                Round 1 - Movie Quotes
             </li>
             <li>
-                Round 2 - History
+                Round 2 - Current Affairs
             </li>
             <li>
-                Round 3 - Geography
+                Round 3 - Goalscorers
             </li>
             <li>
-                Round 4 - Music
+                Round 4 - 2023
             </li>
             <li>
-                Round 5 - Christmas
+                Round 5 - A Mixed Bag
             </li>
             <li>
-                Round 6 - Science
+                Round 6 - Politics
             </li>
             <li>
-                Round 7 - Famous Nicks
+                Round 7 - Food & Drink
             </li>
             <li>
-                Round 8 - Movies
+                {/* Round 8 - Movies */}
             </li>
         </ul>
      );

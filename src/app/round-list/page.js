f31005392591b9569d00cc6910d01c94bb1649c6
Lewis <lewis@westmidlandsmedia.co.uk>
2024-10-18 09:18:08 +0100

    carey quiz live

--- src/app/round-list/page.js
+++ src/app/round-list/page.js
@@ -2,28 +2,28 @@ const RoundList = () => {
     return ( 
         <ul className="page__list">
             <li>
-                Round 1 - Georgraphy
+                Round 1 - Blue
             </li>
             <li>
-                Round 2 - History
+                Round 2 - Girl's Names
             </li>
             <li>
-                Round 3 - Famous Faces
+                Round 3 - Motorways
             </li>
             <li>
-                Round 4 - Space
+                Round 4 - Populations
             </li>
             <li>
-                Round 5 - Food
+                Round 5 - Name the Year
             </li>
             <li>
-                Round 6 - Sport 2023
+                Round 6 - Key Stage 4
             </li>
             <li>
-                Round 7 - The Rich List
+                Round 7 - Sport & Leisure
             </li>
             <li>
-                Round 8 - 2024 General Knowledge
+                Round 8 - London 2012 Olympics Wipeout Round
             </li>
         </ul>
      );

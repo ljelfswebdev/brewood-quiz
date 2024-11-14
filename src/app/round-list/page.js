c602c65f6ea4eebcf590c35f5ad948c2390d6abe
Lewis <lewis@westmidlandsmedia.co.uk>
2024-11-14 18:28:42 +0000

    Carey Quiz nov 2024

--- src/app/round-list/page.js
+++ src/app/round-list/page.js
@@ -2,28 +2,28 @@ const RoundList = () => {
     return ( 
         <ul className="page__list">
             <li>
-                Round 1 - Blue
+                Round 1 - Yellow
             </li>
             <li>
-                Round 2 - Girl's Names
+                Round 2 - 2020
             </li>
             <li>
-                Round 3 - Motorways
+                Round 3 - Words
             </li>
             <li>
-                Round 4 - Populations
+                Round 4 - Breakfast
             </li>
             <li>
-                Round 5 - Name the Year
+                Round 5 - Slogans
             </li>
             <li>
-                Round 6 - Key Stage 4
+                Round 6 - TV & Entertainment
             </li>
             <li>
-                Round 7 - Sport & Leisure
+                Round 7 - Sex
             </li>
             <li>
-                Round 8 - London 2012 Olympics Wipeout Round
+                Round 8 - Wipeout
             </li>
         </ul>
      );

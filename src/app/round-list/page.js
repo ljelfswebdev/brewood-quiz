3c8e39f40045a93ecdd1a22d4da8412ed7391de9
Lewis <lewis@westmidlandsmedia.co.uk>
2024-12-05 18:27:43 +0000

    quiz december 24

--- src/app/round-list/page.js
+++ src/app/round-list/page.js
@@ -2,28 +2,28 @@ const RoundList = () => {
     return ( 
         <ul className="page__list">
             <li>
-                Round 1 - Yellow
+                Round 1 - Dogs
             </li>
             <li>
-                Round 2 - 2020
+                Round 2 - Christmas
             </li>
             <li>
-                Round 3 - Words
+                Round 3 - Childrens Television
             </li>
             <li>
-                Round 4 - Breakfast
+                Round 4 - 1980's History
             </li>
             <li>
-                Round 5 - Slogans
+                Round 5 - India
             </li>
             <li>
-                Round 6 - TV & Entertainment
+                Round 6 - Pink
             </li>
             <li>
-                Round 7 - Sex
+                Round 7 - UK TV
             </li>
             <li>
-                Round 8 - Wipeout
+                Round 8 - Music
             </li>
         </ul>
      );

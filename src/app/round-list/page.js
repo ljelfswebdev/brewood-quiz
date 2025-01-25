42f7ef54ec2fe5b8df0d8817d7cf5a9e45aeeb58
Lewis <ljelfswebdev@gmail.com>
2025-01-25 12:18:03 +0000

    carey quiz

--- src/app/round-list/page.js
+++ src/app/round-list/page.js
@@ -2,28 +2,28 @@ const RoundList = () => {
     return ( 
         <ul className="page__list">
             <li>
-                Round 1 - 1992 Sport
+                Round 1 - Geography
             </li>
             <li>
-                Round 2 - 2020
+                Round 2 - Advertising slogans
             </li>
             <li>
-                Round 3 - 1970's Music
+                Round 3 - The Olympic Games
             </li>
             <li>
-                Round 4 - Scottish History
+                Round 4 - White
             </li>
             <li>
-                Round 5 - Asian Geography
+                Round 5 - Premier League
             </li>
             <li>
-                Round 6 - Purple
+                Round 6 - Children’s TV
             </li>
             <li>
-                Round 7 - Animals of the Sea
+                Round 7 - Disney and movie music
             </li>
             <li>
-                Round 8 - Chocolate
+                Round 8 - General Knowledge
             </li>
         </ul>
      );
